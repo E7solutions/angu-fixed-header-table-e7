2f0827782bea3dc1dd847eecb611cd2425243163
Stefan Goian <stefan.goian@e7solutions.com>
2016-05-19 11:55:14 -0400

    updated bower.json and directive

--- angu-fixed-header-table.js
+++ angu-fixed-header-table.js
@@ -1,6 +1,7 @@
-﻿/**
+/**
  * AngularJS fixed header scrollable table directive
  * @author Jason Watmore <jason@pointblankdevelopment.com.au> (http://jasonwatmore.com)
+ 
  * @version 1.2.0
  */
 (function () {
@@ -8,9 +9,9 @@
         .module('anguFixedHeaderTable', [])
         .directive('fixedHeader', fixedHeader);
 
-    fixedHeader.$inject = ['$timeout'];
+    fixedHeader.$inject = ['$timeout', '$window'];
 
-    function fixedHeader($timeout) {
+    function fixedHeader($timeout, $window) {
         return {
             restrict: 'A',
             link: link
@@ -29,53 +30,98 @@
             function tableDataLoaded() {
                 // first cell in the tbody exists when data is loaded but doesn't have a width
                 // until after the table is transformed
-                var firstCell = elem.querySelector('tbody tr:first-child td:first-child');
+                var firstCell = elem.querySelector('table[fixed-header] > tbody > tr:first-child > td:first-child');
                 return firstCell && !firstCell.style.width;
             }
 
-            function transformTable() {
-                // reset display styles so column widths are correct when measured below
-                angular.element(elem.querySelectorAll('thead, tbody, tfoot')).css('display', '');
+            // make sure to accommodate the scroll bar width if the tbody changes from/to not overflowing
+            $scope.$watch(numRows, function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    transformTable();
+                }
+            });
 
+            function numRows() {
+                return elem.querySelectorAll('table[fixed-header] > tbody > tr').length;
+            }
+
+            function transformTable() {
                 // wrap in $timeout to give table a chance to finish rendering
                 $timeout(function () {
+                    // Instead of doing any calculations on elem, we will do them on a clone.
+                    // This way we won't change the display of the table itself, preventing FOUC when transforming.
+                    var clone = elem.cloneNode(true);
+                    var wrapper = document.createElement('div');
+                    wrapper.style.height = '0px';
+                    wrapper.style.overflow = 'hidden';
+                    wrapper.style.visibility = 'invisible';
+                    wrapper.appendChild(clone);
+                    angular.element(clone.querySelectorAll('thead, tbody, tfoot')).css('display', '');
+                    $elem.after(wrapper);
+
+                    var longestRowBySection = {};
+
                     // set widths of columns
-                    angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
+                    var tableSections = ['tbody', 'thead', 'tfoot'];
+                    for (var i = 0; i < tableSections.length; i++) {
+                        var section = tableSections[i];
+                        var rows = elem.querySelectorAll('table[fixed-header] > ' + section +' > tr');
+                        var cloneRows = clone.querySelectorAll('table[fixed-header] > ' + section +' > tr');
+                        // find the row with the most cells
+                        var mostCells = 0;
+                        longestRowBySection[section] = 0;
+                        for (var j = 0; j < rows.length; j++) {
+                            var cellCount = rows[j].childElementCount;
+                            if (cellCount > mostCells) {
+                                mostCells = cellCount;
+                                longestRowBySection[section] = j;
+                            }
+                        }
+                        var cells = angular.isDefined(rows[longestRowBySection[section]]) ? rows[longestRowBySection[section]].children : [];
+                        for (var k = 0; k < cells.length; k++) {
+                            cells[k].style.width = cloneRows[longestRowBySection[section]].children[k].offsetWidth + 'px';
+                        }
+                    }
 
-                        var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
-                        var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
-
-                        var columnWidth = tdElems ? tdElems.offsetWidth : thElem.offsetWidth;
-                        if (tdElems) {
-                            tdElems.style.width = columnWidth + 'px';
-                        }
-                        if (thElem) {
-                            thElem.style.width = columnWidth + 'px';
-                        }
-                        if (tfElems) {
-                            tfElems.style.width = columnWidth + 'px';
-                        }
-                    });
+                    //Done with the math. We can get rid of the clone.
+                    angular.element(wrapper).remove();
 
                     // set css styles on thead and tbody
-                    angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
-
-                    angular.element(elem.querySelectorAll('tbody')).css({
+                    angular.element(elem.querySelectorAll('table[fixed-header] > thead, table[fixed-header] > tfoot')).css('display', 'block');
+                    angular.element(elem.querySelector('table[fixed-header] > tbody')).css({
                         'display': 'block',
                         'height': $attrs.tableHeight || 'inherit',
+                        'max-height': $attrs.maxTableHeight || 'inherit',
                         'overflow': 'auto'
                     });
 
+                    // Fix tr inheritance of tbody height in IE9.
+                    // This whole directive doesn't actually work in IE9 but this fix
+                    // makes the table look ok when it degrades to a normal table
+                    angular.element(elem.querySelectorAll('table[fixed-header] > tbody > tr')).css('height', 'auto');
+
                     // reduce width of last column by width of scrollbar
-                    var tbody = elem.querySelector('tbody');
+                    var tbody = elem.querySelector('table[fixed-header] > tbody');
                     var scrollBarWidth = tbody.offsetWidth - tbody.clientWidth;
+
                     if (scrollBarWidth > 0) {
-                        // for some reason trimming the width by 2px lines everything up better
-                        scrollBarWidth -= 2;
-                        var lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
+                        // for some reason trimming the width by 4px lines everything up better
+                        scrollBarWidth -= 4;
+                        var lastColumn = elem.querySelector('table[fixed-header] > tbody > tr:nth-child(' + (longestRowBySection.tbody + 1) + ') > td:last-child');
                         lastColumn.style.width = (lastColumn.offsetWidth - scrollBarWidth) + 'px';
                     }
                 });
+            }
+
+            angular.element($window).bind('resize', resizeThrottler);
+            var resizeTimeout;
+            function resizeThrottler() {
+                if (!resizeTimeout) {
+                    resizeTimeout = $timeout(function() {
+                        resizeTimeout = null;
+                        transformTable();
+                    }, 100);
+                }
             }
         }
     }
